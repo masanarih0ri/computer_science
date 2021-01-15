988ad1705a83139c5b387cbe5c2b6d491c444b29
masanarih0ri <masanarihoridevelop@gmail.com>
2021-01-16 02:15:51 +0900

    プレイヤーの手を表示するメソッドを追加

--- js/trumpGame.js
+++ js/trumpGame.js
@@ -55,10 +55,11 @@ class Deck {
 // ディーラーのステートレスオブジェクトを作る
 class Dealer {
   // インスタンス化が必要ないので、static を用いた関数だけかく
-  static startGame(amountOfPlayers) {
-    // tableオブジェクトを設定。プレイヤーの手札とデッキを
+  static startGame(amountOfPlayers, gameMode) {
+    // tableオブジェクトを設定。プレイヤーの手札とゲームの種類、デッキをプロパティとしてもつ
     let table = {
       "playerCards": [],
+      "gameMode": gameMode,
       "deck": new Deck()
     }
     // デッキをシャッフルする
@@ -66,14 +67,44 @@ class Dealer {
 
     for(let i = 0; i < amountOfPlayers; i++) {
       let playerCard = [];
-      for(let j = 0; j < 2; j++) {
+      for(let j = 0; j < Dealer.initialCards(gameMode); j++) {
         playerCard.push(table["deck"].draw());
       }
       table["playerCards"].push(playerCard);
     }
-    return table["playerCards"];
+    return table;
+  }
+  
+  // ゲームをポーカーに拡張できるようにinitialCardsというメソッドを作る
+  static initialCards(gameMode) {
+    if(gameMode === "poker") return 5;
+    if(gameMode === "21") return 2;
+  }
+  
+  // tableの情報を受け取り、プレイヤーのカードを表示するprintTableInformationというメソッドを作る
+  
+  // 以下のように出力してください。
+    // Amount of players: 3... Game mode: 21. At this table:
+    // Player 1 hand is:
+    // ♠55
+    // ♥A1
+    // Player 2 hand is:
+    // ♥66
+    // ♦1010
+    // Player 3 hand is:
+    // ♣J11
+    // ♠33
+  static printTableInformation(table) {
+    console.log(`Amount of players: ${table["playerCards"].length}... Game mode: ${table["gameMode"]}. At this table:`);
+    
+    for(let i = 0; i < table["playerCards"].length; i++) {
+      console.log(`Player ${i + 1} hand is:`);
+      for(let j = 0; j < table["playerCards"][i].length; j++) {
+        console.log(table["playerCards"][i][j].getCardString());
+      }
+    }
   }
 }
 
-let table1 = Dealer.startGame(5);
-console.log(table1)
+let table1 = Dealer.startGame(5, "poker");
+Dealer.printTableInformation(table1);
