6191977b421847923bb34b05d905773d60708a87
masanarih0ri <masanarihoridevelop@gmail.com>
2021-01-27 23:39:17 +0900

    gameModeによる手札の変更を行うロジックを追加

--- js/trumpGame.js
+++ js/trumpGame.js
@@ -11,18 +11,21 @@ class Card {
 }
 
 class Deck {
-  constructor() {
-    this.deck = Deck.generateDeck();
+  constructor(gameMode = null) {
+    this.deck = Deck.generateDeck(gameMode);
   }
   
-  static generateDeck() {
+  static generateDeck(gameMode = null) {
     let newDeck = [];
     const suits = ["♣", "♦", "♥", "♠"];
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let blackjack = {"A": 1, "J": 10, "Q": 10, "K": 10};
     
     for(let i = 0; i < suits.length; i++) {
       for(let j = 0; j < values.length; j++) {
-        newDeck.push(new Card(suits[i], values[j], j + 1));
+        let currentValue = values[j];
+        let intValue = (gameMode === "21") ? (currentValue in blackjack ? blackjack[currentValue] : parseInt(currentValue)) : j + 1;
+        newDeck.push(new Card(suits[i], values[j], intValue));
       }
     }
     return newDeck;
@@ -60,7 +63,7 @@ class Dealer {
     let table = {
       "playerCards": [],
       "gameMode": gameMode,
-      "deck": new Deck()
+      "deck": new Deck(gameMode)
     }
     // デッキをシャッフルする
     table["deck"].shuffleDeck();
