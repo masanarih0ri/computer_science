765a493c6130ec9f1fef0287cff6b3be75b107e3
masanarih0ri <masanarihoridevelop@gmail.com>
2021-03-27 11:55:28 +0900

    サンプル用のファイルを追加

---
+++ project/project1/emotion_onomatopoeia_dictionary/js/prac.js
@@ -0,0 +1,17 @@
+let sectionInfo = [
+  {
+    "title":"Kittens",
+    "color":"green",
+    "description":"A kitten is a juvenile cat. After being born, kittens display primary altriciality and are totally dependent on their mother for survival. They do not normally open their eyes until after seven to ten days. After about two weeks, kittens quickly develop and begin to explore the world outside the nest (Wikipedia)."
+  },
+  {
+    "title":"Tigers",
+    "color":"red",
+    "description":"The tiger is the largest extant cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange-brown fur with a lighter underside. It is an apex predator, primarily preying on ungulates such as deer and wild boar. (Wikipedia)"
+  },
+  {
+    "title":"Elephant",
+    "color":"blue",
+    "description":"Elephants are mammals of the family Elephantidae and the largest existing land animals. Three species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant. Elephantidae is the only surviving family of the order Proboscidea; extinct members include the mastodons. (Wikipedia)"
+  },
+];
