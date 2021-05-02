187a7c7e0c7e0ec43bc971e0131753f5ffd012f0
masanarih0ri <masanarihoridevelop@gmail.com>
2021-05-02 09:49:05 +0900

    スライダーのロジック完成

--- project/project2/slider/js/index.js
+++ project/project2/slider/js/index.js
@@ -1,49 +1,81 @@
-// 全ての箱の要素を取り出し最初の要素を取得する
-let sliderItems = document.querySelectorAll("#target .slider-data .slider-item");
-let firstSliderItem = sliderItems[0];
-console.log(sliderItems);
-console.log(sliderItems[0]);
+const target = document.getElementById("target");
+const sliderItems = document.querySelectorAll("#target .slider-data .slider-item");
 
-// スライダーに関する要素を作る
 let slideShow = document.createElement("div");
 let main = document.createElement("div");
 let extra = document.createElement("div");
 
-// スライダーに関する要素に対してクラスを付与
 slideShow.classList.add("col-12", "d-flex", "flex-nowrap", "overflow-hiddens");
 main.classList.add("main", "full-width");
 extra.classList.add("extra", "full-width");
 
-// スライダーに関する要素に対して必要な要素を追加し、HTMLを構成する
-main.append(firstSliderItem);
+main.append(sliderItems[0]);
+
 slideShow.append(main);
 slideShow.append(extra);
-
-// targetにHTMLを挿入する
-const target = document.getElementById("target");
 target.append(slideShow);
 
-// ボタン関連の要素を作成する
 let controls = document.createElement("div");
-let leftBtn = document.createElement("button");
-let rightBtn = document.createElement("button");
-
-// 必要なクラスをそれぞれの要素に付与する
 controls.classList.add("offset-5", "mt-2");
-leftBtn.classList.add("btn", "btn-light");
-rightBtn.classList.add("btn", "btn-light");
 
-// 必要なHTMLの要素を追加する
+let leftBtn = document.createElement("button");
+leftBtn.classList.add("btn", "btn-light");
 leftBtn.innerHTML = "<";
+
+let rightBtn = document.createElement("button");
+rightBtn.classList.add("btn", "btn-light");
 rightBtn.innerHTML = ">";
 
-// HTMLの構成を作る
 controls.append(leftBtn);
 controls.append(rightBtn);
 
-// targetにHTMLを入れる
 target.append(controls);
 
 //setAttributeを使ってindexの初期値を設定
+main.setAttribute("data-index", "0");
 
-// 1か-1を受け取って次の要素を設定するslideJumpという関数を作成
+// animationTypeというパラメータを追加
+function slideJump(steps, animationType) {
+  let index = parseInt(main.getAttribute("data-index"));
+  // let currentElement = sliderItems[index];
+  let currentElement = sliderItems.item(index);
+
+  index += steps;
+  
+  if(index < 0) index = sliderItems.length - 1;
+  else if(index >= sliderItems.length) index = 0;
+  // let nextElement = sliderItems[index];
+  let nextElement = sliderItems.item(index);
+
+  main.setAttribute("data-index", index.toString());
+  animateMain(currentElement, nextElement, animationType);
+}
+
+function animateMain(current, next, animationType) {
+  main.innerHTML = "";
+  main.append(next);
+
+  extra.innerHTML = "";
+  extra.append(current);
+
+  main.classList.add("expand-animation");
+  extra.classList.add("deplete-animation");
+  
+  if(animationType === "right") {
+    slideShow.innerHTML = "";
+    slideShow.append(extra);
+    slideShow.append(main);
+  } else if (animationType === "left") {
+    slideShow.innerHTML = "";
+    slideShow.append(main);
+    slideShow.append(extra);
+  }
+}
+
+leftBtn.addEventListener("click", function(){
+  slideJump(-1, "left");
+});
+
+rightBtn.addEventListener("click", function(){
+  slideJump(+1, "right");
+});
